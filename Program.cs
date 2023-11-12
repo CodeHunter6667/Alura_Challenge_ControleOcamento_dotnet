7a94ba25f0d174ded30fee8aae7b0e4e3d822b5a
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-12 16:13:41 -0300

    Configuracao de contexto do banco de dados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
+using System.Text.Json.Serialization;
+using AutoMapper;
+using Challenge_2.Context;
+using Challenge_2.DTOs.Mappings;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions
+                .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+var mappingConfig = new MapperConfiguration(mc => 
+    {
+        mc.AddProfile(new MappingProfile());
+    });
+IMapper mapper = mappingConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddEndpointsApiExplorer();
+
+var connectionString = builder.Configuration.GetConnectionString("SqlServerConnection");
+builder.Services.AddDbContext<AppDbContext>(options => 
+                        options.UseSqlServer(connectionString));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
