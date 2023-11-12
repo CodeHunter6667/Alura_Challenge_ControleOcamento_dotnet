a07117cb9079f39a8967076d800e9db69cf4605c
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-12 16:16:05 -0300

    Alterado anotacao para a migracao

--- Models/Despesas.cs
+++ Models/Despesas.cs
@@ -1,7 +1,8 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Challenge_2.Models;
-
+[Table("tb_despesas")]
 public class Despesas
 {
     [Key]
