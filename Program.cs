7bb5ee8b144ff71ff7b076d59626e3ff5cbe6182
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-16 17:42:25 -0300

    CRiado escopo de servico

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using System.Text.Json.Serialization;
 using AutoMapper;
 using Challenge_2.Context;
 using Challenge_2.DTOs.Mappings;
+using Challenge_2.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
                 .AddJsonOptions(options => options.JsonSerializerOptions
                 .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddScoped<ReceitasService>();
 var mappingConfig = new MapperConfiguration(mc => 
     {
         mc.AddProfile(new MappingProfile());
